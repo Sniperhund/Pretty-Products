00217cb13668bf67229ea9ecd6231d23dbf25239
Lucas Täkker <50625016+Sniperhund@users.noreply.github.com>
2024-11-19 13:21:07 +0100

    add initial project setup with Tailwind CSS, Generouted, and Prettier configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "@/styles/global.css"
+import { Button } from "@/components/ui/button"
+import { Routes } from "@generouted/react-router"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<Routes />
+	</StrictMode>
 )
