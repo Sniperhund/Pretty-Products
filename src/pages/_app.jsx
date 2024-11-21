57d14282f63ac590f9d001c72debb569b5df8da6
Lucas Täkker <50625016+Sniperhund@users.noreply.github.com>
2024-11-21 11:45:10 +0100

    add ThemeProvider for managing application theme context

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom"
+import { ThemeProvider } from "@/providers/ThemeProvider"
 
 export default function App() {
 	return (
-		<>
-			App <Outlet />
-		</>
+		<ThemeProvider>
+			<Outlet />
+		</ThemeProvider>
 	)
 }
